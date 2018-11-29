0097806cfe92c0c65321c25e193d81c5c5a6c011
droberts19 <droberts19@scienceleadership.org>
2018-11-29 10:29:59 -0500

    done

--- app/src/main/java/com/example/droberts19/realworlddataproject/Controller.java
+++ app/src/main/java/com/example/droberts19/realworlddataproject/Controller.java
@@ -1,10 +1,12 @@
 package com.example.droberts19.realworlddataproject;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
+import android.view.View;
 import android.widget.ImageView;
 import java.io.File;
 
@@ -17,14 +19,30 @@ public class Controller {
     private TabItem venusTab;
     private TabItem earthTab;
 
-    Controller(ImageView PlanetPics, TabLayout Tabs, TabItem Merc, TabItem Venus, TabItem Earth, final Context c) {
+
+    Controller(ImageView PlanetPics, TabLayout Tabs, final Context c) {
         planets = PlanetPics;
         tabs = Tabs;
-        mercuryTab = Merc;
-        venusTab = Venus;
-        earthTab = Earth;
 
-        mercuryTab.setOn
-        planets.setImageResource(R.drawable.earth);
+        TabLayout tabs = new TabLayout(c);
+        tabs.addTab(tabs.newTab().setText("Mercury"));
+        mercuryTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                planets.setImageResource(R.drawable.mercury);
+            }
+        });
+        venusTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                planets.setImageResource(R.drawable.venus);
+            }
+        });
+        earthTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                planets.setImageResource(R.drawable.earth);
+            }
+        });
     }
 }
