97293926dbf2a8ba2244da5fa5e801e64aef2107
alanli <alli19@scienceleadership.org>
2018-11-30 13:39:59 -0500

    Edit

--- app/src/main/java/com/example/droberts19/realworlddataproject/Planets.java
+++ app/src/main/java/com/example/droberts19/realworlddataproject/Planets.java
@@ -1,5 +1,31 @@
 package com.example.droberts19.realworlddataproject;
 
-public class Planets {
+import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class Planets implements Model {
+    private ArrayList<Planet> planets;
+    private int current;
+
+    // Constructor
+    Planets (Context context) {
+        current = 0;
+        planets = new ArrayList();
+
+        // Read album data from text file.
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getAssets().open("AlbumData")));
+            String nextLine;
+            while ((nextLine = bufferedReader.readLine()) != null) {
+                planets.add(new Planet(nextLine));
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println("Albums() reading data threw exception:");
+            e.printStackTrace();
+        }
+    }
 }
