3dccce0167926a4fcd22d85e753142e3f6ca9126
ADeRock <aderock19@scienceleadership.org>
2018-12-06 10:23:46 -0500

    !

--- app/src/main/java/com/example/droberts19/realworlddataproject/MainActivity2.java
+++ app/src/main/java/com/example/droberts19/realworlddataproject/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.droberts19.realworlddataproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageView iv2 = findViewById(R.id.imageViewer2);
+
     }
 }
