a71806bca20db18739a10b4375bf3b31b6c43ab3
droberts19 <droberts19@scienceleadership.org>
2018-12-07 13:43:58 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/droberts19/realworlddataproject/Planets.java

--- app/src/main/java/com/example/droberts19/realworlddataproject/MainActivity.java
+++ app/src/main/java/com/example/droberts19/realworlddataproject/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.droberts19.realworlddataproject;
 
-import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
